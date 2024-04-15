b41835b8d808907e8aec0065b5b114debdc01742
Wael Karman <wael.karman@gmail.com>
2024-04-15 14:59:21 +0200

    remove pro add cmake

---
+++ build/CMakeFiles/sensor-app.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sensor-app_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/sensor-app_autogen.dir/ParseCache.txt"
+  "sensor-app_autogen"
+  "CMakeFiles/sensor-app.dir/infoexchanger.cpp.o"
+  "CMakeFiles/sensor-app.dir/main.cpp.o"
+  "CMakeFiles/sensor-app.dir/sensor-app_autogen/EWIEGA46WW/qrc_main.cpp.o"
+  "CMakeFiles/sensor-app.dir/sensor-app_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/sensor-app.dir/zmqprocess.cpp.o"
+  "sensor-app"
+  "sensor-app.pdb"
+  "sensor-app_autogen/EWIEGA46WW/qrc_main.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sensor-app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/sensor-app_autogen/EWIEGA46WW/moc_infoexchanger.cpp
@@ -0,0 +1,374 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'infoexchanger.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.6.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "infoexchanger.h"
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'infoexchanger.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.6.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSinfoExchangerENDCLASS_t {};
+constexpr auto qt_meta_stringdata_CLASSinfoExchangerENDCLASS = QtMocHelpers::stringData(
+    "infoExchanger",
+    "priorityChanged0",
+    "",
+    "priorityChanged1",
+    "priorityChanged2",
+    "priorityChanged3",
+    "priorityChanged4",
+    "priorityChanged5",
+    "priorityChanged6",
+    "m_value0",
+    "m_value1",
+    "m_value2",
+    "m_value3",
+    "m_value4",
+    "m_value5",
+    "m_value6"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSinfoExchangerENDCLASS_t {
+    uint offsetsAndSizes[32];
+    char stringdata0[14];
+    char stringdata1[17];
+    char stringdata2[1];
+    char stringdata3[17];
+    char stringdata4[17];
+    char stringdata5[17];
+    char stringdata6[17];
+    char stringdata7[17];
+    char stringdata8[17];
+    char stringdata9[9];
+    char stringdata10[9];
+    char stringdata11[9];
+    char stringdata12[9];
+    char stringdata13[9];
+    char stringdata14[9];
+    char stringdata15[9];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSinfoExchangerENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSinfoExchangerENDCLASS_t qt_meta_stringdata_CLASSinfoExchangerENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 13),  // "infoExchanger"
+        QT_MOC_LITERAL(14, 16),  // "priorityChanged0"
+        QT_MOC_LITERAL(31, 0),  // ""
+        QT_MOC_LITERAL(32, 16),  // "priorityChanged1"
+        QT_MOC_LITERAL(49, 16),  // "priorityChanged2"
+        QT_MOC_LITERAL(66, 16),  // "priorityChanged3"
+        QT_MOC_LITERAL(83, 16),  // "priorityChanged4"
+        QT_MOC_LITERAL(100, 16),  // "priorityChanged5"
+        QT_MOC_LITERAL(117, 16),  // "priorityChanged6"
+        QT_MOC_LITERAL(134, 8),  // "m_value0"
+        QT_MOC_LITERAL(143, 8),  // "m_value1"
+        QT_MOC_LITERAL(152, 8),  // "m_value2"
+        QT_MOC_LITERAL(161, 8),  // "m_value3"
+        QT_MOC_LITERAL(170, 8),  // "m_value4"
+        QT_MOC_LITERAL(179, 8),  // "m_value5"
+        QT_MOC_LITERAL(188, 8)   // "m_value6"
+    },
+    "infoExchanger",
+    "priorityChanged0",
+    "",
+    "priorityChanged1",
+    "priorityChanged2",
+    "priorityChanged3",
+    "priorityChanged4",
+    "priorityChanged5",
+    "priorityChanged6",
+    "m_value0",
+    "m_value1",
+    "m_value2",
+    "m_value3",
+    "m_value4",
+    "m_value5",
+    "m_value6"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSinfoExchangerENDCLASS[] = {
+
+ // content:
+      12,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       7,   14, // methods
+       7,   63, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       7,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   56,    2, 0x06,    8 /* Public */,
+       3,    0,   57,    2, 0x06,    9 /* Public */,
+       4,    0,   58,    2, 0x06,   10 /* Public */,
+       5,    0,   59,    2, 0x06,   11 /* Public */,
+       6,    0,   60,    2, 0x06,   12 /* Public */,
+       7,    0,   61,    2, 0x06,   13 /* Public */,
+       8,    0,   62,    2, 0x06,   14 /* Public */,
+
+ // signals: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+ // properties: name, type, flags
+       9, QMetaType::QString, 0x00015001, uint(0), 0,
+      10, QMetaType::QString, 0x00015001, uint(1), 0,
+      11, QMetaType::QString, 0x00015003, uint(2), 0,
+      12, QMetaType::QString, 0x00015001, uint(3), 0,
+      13, QMetaType::QString, 0x00015001, uint(4), 0,
+      14, QMetaType::QString, 0x00015001, uint(5), 0,
+      15, QMetaType::QString, 0x00015001, uint(6), 0,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject infoExchanger::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_meta_stringdata_CLASSinfoExchangerENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSinfoExchangerENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSinfoExchangerENDCLASS_t,
+        // property 'm_value0'
+        QtPrivate::TypeAndForceComplete<QString, std::true_type>,
+        // property 'm_value1'
+        QtPrivate::TypeAndForceComplete<QString, std::true_type>,
+        // property 'm_value2'
+        QtPrivate::TypeAndForceComplete<QString, std::true_type>,
+        // property 'm_value3'
+        QtPrivate::TypeAndForceComplete<QString, std::true_type>,
+        // property 'm_value4'
+        QtPrivate::TypeAndForceComplete<QString, std::true_type>,
+        // property 'm_value5'
+        QtPrivate::TypeAndForceComplete<QString, std::true_type>,
+        // property 'm_value6'
+        QtPrivate::TypeAndForceComplete<QString, std::true_type>,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<infoExchanger, std::true_type>,
+        // method 'priorityChanged0'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'priorityChanged1'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'priorityChanged2'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'priorityChanged3'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'priorityChanged4'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'priorityChanged5'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'priorityChanged6'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void infoExchanger::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<infoExchanger *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->priorityChanged0(); break;
+        case 1: _t->priorityChanged1(); break;
+        case 2: _t->priorityChanged2(); break;
+        case 3: _t->priorityChanged3(); break;
+        case 4: _t->priorityChanged4(); break;
+        case 5: _t->priorityChanged5(); break;
+        case 6: _t->priorityChanged6(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (infoExchanger::*)();
+            if (_t _q_method = &infoExchanger::priorityChanged0; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (infoExchanger::*)();
+            if (_t _q_method = &infoExchanger::priorityChanged1; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 1;
+                return;
+            }
+        }
+        {
+            using _t = void (infoExchanger::*)();
+            if (_t _q_method = &infoExchanger::priorityChanged2; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 2;
+                return;
+            }
+        }
+        {
+            using _t = void (infoExchanger::*)();
+            if (_t _q_method = &infoExchanger::priorityChanged3; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 3;
+                return;
+            }
+        }
+        {
+            using _t = void (infoExchanger::*)();
+            if (_t _q_method = &infoExchanger::priorityChanged4; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 4;
+                return;
+            }
+        }
+        {
+            using _t = void (infoExchanger::*)();
+            if (_t _q_method = &infoExchanger::priorityChanged5; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 5;
+                return;
+            }
+        }
+        {
+            using _t = void (infoExchanger::*)();
+            if (_t _q_method = &infoExchanger::priorityChanged6; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 6;
+                return;
+            }
+        }
+    } else if (_c == QMetaObject::ReadProperty) {
+        auto *_t = static_cast<infoExchanger *>(_o);
+        (void)_t;
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: *reinterpret_cast< QString*>(_v) = _t->getvalue0(); break;
+        case 1: *reinterpret_cast< QString*>(_v) = _t->getvalue1(); break;
+        case 2: *reinterpret_cast< QString*>(_v) = _t->getvalue2(); break;
+        case 3: *reinterpret_cast< QString*>(_v) = _t->getvalue3(); break;
+        case 4: *reinterpret_cast< QString*>(_v) = _t->getvalue4(); break;
+        case 5: *reinterpret_cast< QString*>(_v) = _t->getvalue5(); break;
+        case 6: *reinterpret_cast< QString*>(_v) = _t->getvalue6(); break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::WriteProperty) {
+        auto *_t = static_cast<infoExchanger *>(_o);
+        (void)_t;
+        void *_v = _a[0];
+        switch (_id) {
+        case 2: _t->setvalue2(*reinterpret_cast< QString*>(_v)); break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::ResetProperty) {
+    } else if (_c == QMetaObject::BindableProperty) {
+    }
+    (void)_a;
+}
+
+const QMetaObject *infoExchanger::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *infoExchanger::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSinfoExchangerENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int infoExchanger::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 7)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 7;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 7)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 7;
+    }else if (_c == QMetaObject::ReadProperty || _c == QMetaObject::WriteProperty
+            || _c == QMetaObject::ResetProperty || _c == QMetaObject::BindableProperty
+            || _c == QMetaObject::RegisterPropertyMetaType) {
+        qt_static_metacall(this, _c, _id, _a);
+        _id -= 7;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void infoExchanger::priorityChanged0()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+
+// SIGNAL 1
+void infoExchanger::priorityChanged1()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, nullptr);
+}
+
+// SIGNAL 2
+void infoExchanger::priorityChanged2()
+{
+    QMetaObject::activate(this, &staticMetaObject, 2, nullptr);
+}
+
+// SIGNAL 3
+void infoExchanger::priorityChanged3()
+{
+    QMetaObject::activate(this, &staticMetaObject, 3, nullptr);
+}
+
+// SIGNAL 4
+void infoExchanger::priorityChanged4()
+{
+    QMetaObject::activate(this, &staticMetaObject, 4, nullptr);
+}
+
+// SIGNAL 5
+void infoExchanger::priorityChanged5()
+{
+    QMetaObject::activate(this, &staticMetaObject, 5, nullptr);
+}
+
+// SIGNAL 6
+void infoExchanger::priorityChanged6()
+{
+    QMetaObject::activate(this, &staticMetaObject, 6, nullptr);
+}
+QT_WARNING_POP
